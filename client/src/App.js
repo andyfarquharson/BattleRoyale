6d37ab88e63b196b8f73793f3c2a652e9ac3516c
Andy Farquharson <andyfarquharson@gmail.com>
2023-01-14 14:13:50 -0800

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,160 @@
+import "./App.scss";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
+const MAX_X_BOARDER = 1344;
+const MAX_Y_BOARDER = 736;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function App() {
+  const [players, setPlayers] = useState({});
+  const [playerId, setPlayerId] = useState(null);
+  const [playerDirection, setPlayerDirection] = useState("down");
+  const [playerPosition, setPlayerPosition] = useState({
+    x: getRandomInt(800),
+    y: getRandomInt(600),
+  });
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setPlayerId(socket.id);
+      setPlayers((prevPlayers) => {
+        return {
+        ...prevPlayers,
+        [socket.id]: { x: playerPosition.x, y: playerPosition.y, health: 100, direction: playerDirection },
+        }
+      });
+      socket.emit("newPlayer", {
+        id: socket.id,
+        x: playerPosition.x,
+        y: playerPosition.y,
+        direction: playerDirection
+      });
+    });
+
+    // Update the position of other players when they move
+    socket.on("playerMoved", (data) => {
+      setPlayers((prevPlayers) => ({
+        ...prevPlayers,
+        [data.id]: { ...prevPlayers[data.id], x: data.x, y: data.y },
+      }));
+    });
+
+    // Update the player health when they are hit
+    socket.on("playerHit", (id) => {
+      setPlayers((prevPlayers) => {
+        let newPlayers = { ...prevPlayers };
+        newPlayers[id].health -= 25;
+        return newPlayers;
+      });
+    });
+
+    // Remove the player when they are killed
+    socket.on("playerKilled", (id) => {
+      setPlayers((prevPlayers) => {
+        let newPlayers = { ...prevPlayers };
+        delete newPlayers[id];
+        return newPlayers;
+      });
+    });
+
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [players]);
+
+  function handleKeyPress(event) {
+    if (event.keyCode === 37) {
+      movePlayer("left");
+      //changeSprite('left')
+      console.log("Left");
+    } else if (event.keyCode === 38) {
+      movePlayer("up");
+      console.log("Up");
+    } else if (event.keyCode === 39) {
+      movePlayer("right");
+      console.log("Right");
+    } else if (event.keyCode === 40) {
+      movePlayer("down");
+      console.log("Down");
+    }
+  }
+  function movePlayer(direction) {
+    let newX = playerPosition.x;
+    let newY = playerPosition.y;
+   
+    if (direction === "left" && playerPosition.x > 32) {
+        newX -= 16;
+    } else if (playerPosition.y > 144 && direction === "up") {
+      newY -= 16;
+    } else if (playerPosition.x < MAX_X_BOARDER && direction === "right") {
+      newX += 16;
+    } else if (playerPosition.y < MAX_Y_BOARDER && direction === "down") {
+      newY += 16;
+    }
+    setPlayerDirection(direction)
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [playerId]: { ...prevPlayers[playerId], x: newX, y: newY },
+      };
+    });
+    setPlayerPosition({ x: newX, y: newY });
+    socket.emit("move", { x: newX, y: newY, playerDirection });
+    console.log("newX, newY:", newX, newY);
+    console.log("x, y, direction", playerPosition.x, playerPosition.y, playerDirection);
+  }
+
+  // Send the player's position to the server when they move
+  function handleMove(x, y) {
+    socket.emit("move", { x, y });
+  }
+
+  // Send the player's attack to the server when they attack
+  function handleAttack(x, y) {
+    socket.emit("attack", { x, y });
+  }
+  function updatePlayerPosition(data) {
+    setPlayers({
+      ...players,
+      [data.id]: { ...players[data.id], x: data.x, y: data.y },
+    });
+  }
+
+  function updatePlayerHealth(id) {
+    setPlayers({
+      ...players,
+      [id]: { ...players[id], health: players[id].health - 25 },
+    });
+  }
+
+  function removePlayer(id) {
+    let newPlayers = { ...players };
+    delete newPlayers[id];
+    setPlayers(newPlayers);
+  }
+
+  return (
+    <div className="App">
+      <div className="game-board">
+        {Object.values(players).map((player, i) => (
+          <div
+            key={player.id}
+            className="player"
+            id={`player-${i}`}
+            style={{ left: `${player.x}px`, top: `${player.y}px` 
+          
+          }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,73 @@
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+server.listen(3001, () => {
+  console.log("Server is running on Port: 3001");
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["GET", "POST"],
+  },
+});
+
+let players = {};
+
+io.on("connection", (socket) => {
+  console.log("A player has connected: ", socket.id);
+
+  // Add the new player to the game state
+  players[socket.id] = { x: 0, y: 0, health: 100 };
+
+  socket.on("move", (data) => {
+    // Update the player's position in the game state
+    players[socket.id].x = data.x;
+    players[socket.id].y = data.y;
+
+    // Broadcast the updated position to all other players
+    socket.broadcast.emit("playerMoved", {
+      id: socket.id,
+      x: data.x,
+      y: data.y,
+    });
+  });
+
+  socket.on("attack", (data) => {
+    // Check if the attack hit another player
+    for (let id in players) {
+      if (id !== socket.id) {
+        let player = players[id];
+        let dx = player.x - data.x;
+        let dy = player.y - data.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < 50) {
+          // Reduce the hit player's health
+          player.health -= 25;
+          if (player.health <= 0) {
+            // Broadcast the player is killed, and remove the player from the game state
+            io.emit("playerKilled", id);
+            delete players[id];
+          } else {
+            // Broadcast the player is hit
+            io.emit("playerHit", id);
+          }
+        }
+      }
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A player has disconnected: ", socket.id);
+    // Remove the player from the game state
+    delete players[socket.id];
+  });
+});
